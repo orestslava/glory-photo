75ee583873dd1e127b9926e58780a1c588b8ee17
orestslava <slavaorest@gmail.com>
2024-08-17 16:03:51 +0100

    grid photos

--- app/gallery/[postId]/page.jsx
+++ app/gallery/[postId]/page.jsx
@@ -3,6 +3,23 @@
 import Image from "next/image"
 import { useState, useEffect } from "react"
 
+function splitArrayIntoThreeEqualParts(array) {
+  const totalObjects = array.length
+  const size = Math.floor(totalObjects / 3)
+  const remainder = totalObjects % 3
+
+  let result = [[], [], []]
+  let startIndex = 0
+
+  for (let i = 0; i < 3; i++) {
+    let currentSize = size + (i < remainder ? 1 : 0)
+    result[i] = array.slice(startIndex, startIndex + currentSize)
+    startIndex += currentSize
+  }
+
+  return result
+}
+
 const GalleryPost = ({ params }) => {
   const folderId = params.postId
   const [fileCount, setFileCount] = useState(null)
@@ -21,18 +38,23 @@ const GalleryPost = ({ params }) => {
     fetchFileCount()
   }, [folderId])
 
-  const arr = Array.from({ length: fileCount }, (_, i) => ({ id: i + 1, image: `/assets/images/gallery/${folderId}/${i + 1}.jpg` }))
-  console.log(arr)
+  const arr = splitArrayIntoThreeEqualParts(
+    Array.from({ length: fileCount }, (_, i) => ({ id: i + 1, image: `/assets/images/gallery/${folderId}/${i + 1}.jpg` }))
+  )
 
   return (
     <div className="pt-[105px]">
       <div className="container my-10 md:px-20">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 auto-rows-max">
-          {arr.map(num => (
-            <div key={num.id} className="relative">
-              <div className="w-full h-[400px] ">
-                <Image src={num.image} alt="fs" layout="fill" objectFit="cover" className="hover:scale-105 transition-all duration-700" />
-              </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
+          {arr.map((col, i) => (
+            <div key={i} className="flex flex-col gap-2 relative w-full h-full ">
+              {col.map(colItem => (
+                <div key={colItem.id} className="overflow-hidden group">
+                  <div className="w-full full transform transition duration-700 ease-in group-hover:scale-110">
+                    <Image src={colItem.image} alt="fs" width={0} height={0} className="" sizes="100vw" style={{ width: "100%", height: "100%" }} />
+                  </div>
+                </div>
+              ))}
             </div>
           ))}
         </div>
